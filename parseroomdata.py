9277c799a70fc1a63dd3528d883623bbf4fc667a
Kieran Weaver <drawbook.productions@gmail.com>
2020-04-01 21:47:09 -0700

    Add basic battle system and upload parseroomdata.py

---
+++ parseroomdata.py
@@ -0,0 +1,264 @@
+import re
+import os
+import json
+import ast
+import string
+import collections
+def PrintRoom(roomdict):
+	numbers = ['zero','one','two','three','four','five','six','seven','eight','nine']
+	roomname = re.sub(' ','_',roomdict['name']).lower()
+	roomname = re.sub('^([0-9]+)',lambda match: ''.join([numbers[int(x)] for x in match.group(1)]),roomname)
+	printable = set(string.printable)
+	roomname = "".join([x for x in roomname if x in printable])
+	keys = ['name','exits','info','npcs','region']
+	roomstr = ''
+	roomstr += roomname
+	roomstr += ' = Room('
+	for i in keys:
+		roomstr += repr(roomdict[i])
+		roomstr += ',\n'
+	roomstr += repr(roomdict['spawner'])
+	roomstr += ')'
+	return roomstr
+		
+def GenRoomJSON(room):
+	roomdict = {
+		'name' : '',
+		'exits': [],
+		'info' : '',
+		'npcs' : [],
+		'region': '',
+		'spawner': '',
+#		'players': []
+		'items' : []
+	}
+	literal_room = ast.literal_eval(room)
+	new_exits = []
+	if literal_room[2] != None:
+		exits = set(literal_room[1])
+		dexits = set(literal_room[2])
+		new_exits = exits - dexits
+		literal_room[2].extend(list(new_exits))
+		roomdict['exits'] = literal_room[2]
+	else:
+		roomdict['exits'] = literal_room[1]
+	roomdict['name'] = literal_room[0]
+	roomdict['info'] = literal_room[3]
+	roomdict['npcs'] = literal_room[4]
+	roomdict['region'] = literal_room[5]
+	roomdict['items'] = []
+	try:
+		roomdict['spawner'] = literal_room[6]
+	except:
+		roomdict['spawner'] = None
+	return (PrintRoom(roomdict),roomdict)
+
+def GenRespawnJSON(spawnpoints,rmessages):
+	regions = spawnpoints.keys()
+	rdata = {}
+	for key in regions:
+		rdata[key] = {'room':spawnpoints[key],'message':rmessages[key]}
+	return rdata
+
+def GenBusJSON(busdict):
+	bdata = {}
+	for k in busdict.keys():
+		for v in busdict[k]:
+			if not v in bdata.keys():
+				bdata[v] = []
+			bdata[v].append(k)
+	return bdata
+
+roomdata = ""
+auxdata = ""
+#npclist = '["Box Robot", "Student", "Seagull", "Pigeon", "Crow", "Cookie", "Diwheel"]'
+with open("rqdata/episodes/episode1.py",'r') as room_file:
+    roomdata += '\n\n'
+    roomdata += room_file.read()
+    npclist_match = re.search('npclist = (\[.*\])',roomdata)
+    roomdata = re.sub(r'npclist,',str(npclist_match.group(1))+',',roomdata)
+
+
+with open("rqdata/rooms.py",'r') as room_file:
+    auxdata = room_file.read()
+
+roomdata = re.sub(r'npclist = (\[.*\])','',roomdata)
+busdata = auxdata
+respawndata = auxdata
+roomdata = re.sub(r'.*utp_lounge','utp_lounge',roomdata,count=1,flags=re.DOTALL)
+roomdata = re.sub(r'busstops = .*','',roomdata,count=1,flags=re.DOTALL)
+roomdata = re.sub(r'#.*','',roomdata)
+roomdata = re.sub(r"'''.*'''",'',roomdata,flags=re.DOTALL)
+
+busdata = re.sub(r'.*busstops = ','',busdata,flags = re.DOTALL)
+busdata = re.sub(os.linesep + 'respawn.*','',busdata,flags=re.DOTALL)
+
+respawndata = re.sub('.*' + os.linesep + 'respawn = ','',respawndata,flags=re.DOTALL)
+respawndata = re.sub(r'def findroom.*','',respawndata,flags=re.DOTALL)
+respawnlists = [ast.literal_eval(x) for x in respawndata.split(os.linesep + 'rmessage = ')]
+
+rooms = list(collections.OrderedDict.fromkeys(roomdata.split(os.linesep + os.linesep)))
+rooms = [s for s in rooms if not "class Room" in s]
+rooms = [re.sub(r'.*= Room','',room.strip(),flags=re.DOTALL) for room in rooms if room.strip()]
+#rooms = [re.sub(r'npclist',npclist,room) for room in rooms]
+rooms = [re.sub(r'\(','[',room) for room in rooms]
+rooms = [re.sub(r'"\)','"]',room) for room in rooms]
+rooms = [re.sub(r'\n','',room) for room in rooms]
+rooms = [re.sub(r'"\+"\\n"\.join\[([^\)]*)\)',lambda match: eval('"\\\\n".join(' + match.group(1) + ')') + '\"', room) for room in rooms]
+
+gamedata = {
+	'rooms' : {
+	},
+	'respawn' : {
+	},
+	'fast-travel' : {
+	},
+	'players' : {
+	},
+	'npcs' : {
+		"Diwheel": {
+			"name" : "DiWheel",
+			"hp" : 20,
+			"atk" : 1,
+			"def" : 0,
+			"xp" : 1,
+			"text": {
+				"entry" : "Jim is in the room, driving his DiWheel robot towards you",
+				"turn" : "DiWheel used quick attack! It isn't very effective",
+				"win" : "I think I heard Irena scream in the distance",
+				"lose" : "You blacked out! Jim put your body in a sleeping bag and mailed you home."
+			}
+		},
+		"Box Robot": {
+			"name" : "Box Robot",
+			"hp" : 10,
+			"atk" : 4,
+			"def" : 0,
+			"xp" : 5,
+			"text": {
+				"entry" : "There is a box-shaped robot on the floor.\n It's staring at you menacingly",
+				"turn" : "The robot charges at you and tries to knock you down",
+				"win" : "The robot violently exploded. Nice.",
+				"lose" : "You blacked out! The robot taunts you, spinning in circles"
+			}
+		},
+		"Student": {
+			"name" : "Student",
+			"hp" : 40,
+			"atk" : 2,
+			"def" : 0,
+			"xp" : 20,
+			"text": {
+				"entry": "Tobias fell out of the sky and his arms returned to their normal size.",
+				"turn": "Tobias shot lightning at you, but you are wearing a metal jacket",
+				"win": "You're already a better fighter than most of the Agency",
+				"lose": """ "You're still so weak, Laura," Tobias said """
+			}
+		},
+		"Seagull": {
+			"name" : "Seagull",
+			"hp" : 5,
+			"atk" : 8,
+			"def" : 2,
+			"xp" : 2,
+			"text": {
+				"entry": "A seagull decided you tasted better than some guy's fries.",
+				"turn": "Birds are jerks! It rammed right into your face",
+				"win": "Now you can have lunch in peace",
+				"lose": "You blacked out!"
+			}
+		},
+		"Pigeon": {
+			"name" : "Pidgeon",
+			"hp" : 60,
+			"atk" : 10,
+			"def" : 0,
+			"xp" : 20,
+			"text": {
+				"entry": "You're approaching me?",
+				"turn": "Instead of running away, you're coming closer?",
+				"win": "Why are all the bosses birds, anyway",
+				"lose": "This isn't a pidgeon! It's me, DIO!"
+			}
+		},
+		"Crow": {
+			"name" : "Crow",
+			"hp" : 100,
+			"atk" : 15,
+			"def" : 0,
+			"xp" : 40,
+			"text": {
+				"entry": "There's a crow the size of a cow sitting in front of you",
+				"turn": "It attempted to crush you under its massive weight",
+				"win": "Stop this animal abuse!",
+				"lose": "You blacked out! Wait until items are implemented next time"
+			}
+		},
+		"Cookie": {
+			"name" : "Cookie",
+			"hp" : 2,
+			"atk" : 255,
+			"def" : 0,
+			"xp" : 0,
+			"text": {
+				"entry": "It's a cookie, how tough could it be?",
+				"turn": "IT GREW FOUR HUNDRED EYES AND THEY ALL STARED AT YOU",
+				"win": "See? Good thing you defeated it quickly...",
+				"lose": "You blacked out and it bit you as revenge"
+			}
+		}
+	}
+}
+items = {
+	'soup' : {
+		'type' : 'edible',
+		'cost' : 0,
+		'hp' : 0,
+		'sp' : 10,
+		'atk' : 0,
+		'status' : {}
+	},
+	'health_soup' : {
+		'type' : 'edible',
+		'cost' : 0,
+		'hp' : 10,
+		'sp' : 0,
+		'atk' : 0,
+		'status' : {}
+	},
+	'souper_soup' : {
+		'type' : 'edible',
+		'cost' : 50,
+		'hp' : 50,
+		'sp' : 50,
+		'atk' : 1,
+		'status' : {}
+	}
+}
+with open("fullrooms.py","w") as fullroomfile:
+	fullroomfile.write("""class Room(object):
+  _registry = []
+  
+  def __init__(self, name, exits, info, npcs, region, spawner = None):
+    self.name = name
+    self.exits = exits
+    self.info = info
+    self.npcs = npcs
+    self.region = region
+    self.spawner = spawner
+    self._registry.append(self)
+    
+""")
+	for room in rooms:
+		tmp = GenRoomJSON(room)
+		gamedata['rooms'][tmp[1]['name']] = tmp[1]
+		fullroomfile.write(tmp[0])
+		fullroomfile.write('\n\n')
+
+gamedata['respawn'] = GenRespawnJSON(respawnlists[0],respawnlists[1])
+
+gamedata['fast-travel'] = GenBusJSON(ast.literal_eval(busdata))
+
+with open("rooms.json","w") as room_json:
+	json.dump(gamedata,room_json,indent=2)
+	
--- rqstate.py
+++ rqstate.py
@@ -1,16 +1,23 @@
 import json
 import collections
 import threading
+import time
 DEBUG = 1
 #PlayerState is dynamic data, the saved json is static data
+healCost = 20
+attackCost = 0
+blockCost = 5
 class PlayerState:
-	hp = 100
-	sp = 100
+	hp = 100 # Goes down over time in battle
+	sp = 100 # For using "attack", "block", and "heal"
+	maxHP = 100
+	atk = 4
 	xp = 0
 	money = 0
 	location = 'UTP Lounge'
 	items = {}
-	status = {}
+	state = "map"
+	status = {"battle" : {}}
 
 class RQState:
 	savedData = {}
@@ -29,7 +36,7 @@ class RQState:
 		for player in self.players:
 			self.savePlayer(player)
 		with open(filename,"w") as f:
-			json.dump(self.savedData,f)
+			json.dump(self.savedData,f,indent=2)
 			
 	def writeMessage(self,message):
 		self.oqlock.acquire()
@@ -45,7 +52,6 @@ class RQState:
 		self.savePlayer(playerid)
 		
 	def loadPlayer(self,playerid):
-		self.players[playerid] = PlayerState()
 		self.players[playerid].hp = self.savedData['players'][playerid]['hp']
 		self.players[playerid].sp = self.savedData['players'][playerid]['sp']
 		self.players[playerid].xp = self.savedData['players'][playerid]['xp']
@@ -73,12 +79,62 @@ class RQState:
 		else:
 			self.writeMessage("That is not an exit")
 
+	def handleRoom(self, playerid):
+		room = self.savedData['rooms'][self.players[playerid].location]
+		if (room["npcs"]):
+			self.players[playerid].status["battle"] = self.savedData["npcs"][room["npcs"][0]].copy()
+			self.players[playerid].state = "battle"
+			self.oqlock.acquire()
+			self.outqueue.append(self.players[playerid].status["battle"]["text"]["entry"])
+			self.oqlock.release()
+
+	def handleBattle(self,playerid, message):
+		messagelist = message.split()
+		if ((messagelist[0] == "attack") and (self.players[playerid].sp >= attackCost)):
+			self.players[playerid].status["battle"]["hp"] -= (self.players[playerid].atk - self.players[playerid].status["battle"]["def"])
+			self.players[playerid].sp -= attackCost
+		elif ((messagelist[0] == "heal") and (self.players[playerid].sp >= healCost)):
+			self.players[playerid].hp = self.players[playerid].maxHP
+			self.players[playerid].sp -= healCost
+		if (self.players[playerid].status["battle"]["hp"] < 0):
+			self.oqlock.acquire()
+			self.outqueue.append(self.players[playerid].status["battle"]["text"]["win"])
+			self.oqlock.release()
+			self.players[playerid].status["battle"] = {}
+			self.players[playerid].state = "map"
+			self.players[playerid].sp = 100
+			self.players[playerid].hp = self.players[playerid].maxHP
+			return
+		defMul = 1.0
+		if ((messagelist[0] == "block") and (self.players[playerid].sp >= blockCost)):
+			defMul = 0.5
+			self.players[playerid].sp -= blockCost
+		self.players[playerid].hp -= defMul*self.players[playerid].status["battle"]["atk"]
+		self.oqlock.acquire()
+		if (self.players[playerid].hp <= 0):
+			self.outqueue.append(self.players[playerid].status["battle"]["text"]["lose"])
+			self.oqlock.release()
+			self.players[playerid].state = "map"
+			self.killPlayer(playerid)
+			return
+		else:
+			self.outqueue.append(self.players[playerid].status["battle"]["text"]["turn"])
+			self.oqlock.release()
+		self.printState(playerid)
+		self.printNPC(playerid)
+		
 	def printRoom(self,playerid):
 		room = self.savedData['rooms'][self.players[playerid].location]
 		rmessage = [room['name'],room['info'],'Items:'] + room['items']
 #		rmessage = rmessage + ['People Here:'] + rooms['players']
 		rmessage.append('Region: ' + room['region'])
 		self.writeMessage("\n".join(rmessage))
+	
+	def printNPC(self, playerid):
+		self.oqlock.acquire()
+		self.outqueue.append("NPC Name: " + self.players[playerid].status["battle"]["name"])
+		self.outqueue.append("NPC HP: {}".format(self.players[playerid].status["battle"]["hp"]))
+		self.oqlock.release()
 		
 	def killPlayer(self,playerid):
 		room = self.savedData['rooms'][self.players[playerid].location]
@@ -89,6 +145,13 @@ class RQState:
 		self.players[playerid].location = self.savedData['respawn'][room['region']]['room']
 		self.writeMessage(self.savedData['respawn'][room['region']]['message'])
 
+	def printState(self, playerid):
+		player = self.players[playerid]
+		self.oqlock.acquire()
+		self.outqueue.append("HP: {}".format(player.hp))
+		self.outqueue.append("SP: {}".format(player.sp))
+		self.oqlock.release()
+		
 	def printInventory(self,playerid):
 		self.oqlock.acquire()
 		self.outqueue.append("Money: {}".format(self.players[playerid].money))
@@ -125,12 +188,20 @@ class RQState:
 			elif (messagelist[0] == "modmoney"):
 				self.players[playerid].money += int(messagelist[1])
 				return
-		if (messagelist[0] == "ride"):
-			messagelist.pop(0)
-			self.fastTravel(playerid,message[5:])
-		elif (messagelist[0][:4] == 'inv'):
-			self.printInventory(playerid)
-		elif (messagelist[0] == "panic"):
-			self.killPlayer(playerid)
-		else:
-			self.movePlayer(playerid,message)
+		if (self.players[playerid].state == "map"):
+			if (messagelist[0] == "ride"):
+				messagelist.pop(0)
+				self.fastTravel(playerid,message[5:])
+			elif (messagelist[0][:4] == 'inv'):
+				self.printInventory(playerid)
+			elif (messagelist[0] == "panic"):
+				self.killPlayer(playerid)
+			else:
+				self.movePlayer(playerid,message)
+				self.handleRoom(playerid)
+		elif (self.players[playerid].state == "battle"):
+			if (messagelist[0][:4] == "inv"):
+				self.printState(playerid)
+				self.printInventory(playerid)
+			else:
+				self.handleBattle(playerid, message)
