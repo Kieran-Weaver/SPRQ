795083abdbcdedb196db770982cc49186434f450
Kieran Weaver <drawbook.productions@gmail.com>
2020-04-25 23:33:20 -0700

    Refactor parseroomdata.py and add gitignore

--- parseroomdata.py
+++ parseroomdata.py
@@ -4,6 +4,9 @@ import json
 import ast
 import string
 import collections
+import inspect
+import rqdata.rooms
+
 itemtable = {
 	"🍰" : "cake",
 	"🥘" : "soup",
@@ -55,54 +58,29 @@ itemtable = {
 	"🥛" : "milk",
 	"❤️" : "heart"
 }
-def PrintRoom(roomdict):
-	numbers = ['zero','one','two','three','four','five','six','seven','eight','nine']
-	roomname = re.sub(' ','_',roomdict['name']).lower()
-	roomname = re.sub('^([0-9]+)',lambda match: ''.join([numbers[int(x)] for x in match.group(1)]),roomname)
-	printable = set(string.printable)
-	roomname = "".join([x for x in roomname if x in printable])
-	keys = ['name','exits','info','npcs','region']
-	roomstr = ''
-	roomstr += roomname
-	roomstr += ' = Room('
-	for i in keys:
-		roomstr += repr(roomdict[i])
-		roomstr += ',\n'
-	roomstr += repr(roomdict['spawner'])
-	roomstr += ')'
-	return roomstr
-		
+
 def GenRoomJSON(room):
-	roomdict = {
-		'name' : '',
-		'exits': [],
-		'info' : '',
-		'npcs' : [],
-		'region': '',
-		'spawner': '',
-#		'players': []
-		'items' : []
-	}
-	literal_room = ast.literal_eval(room)
-	new_exits = []
-	if literal_room[2] != None:
-		exits = set(literal_room[1])
-		dexits = set(literal_room[2])
-		new_exits = exits - dexits
-		literal_room[2].extend(list(new_exits))
-		roomdict['exits'] = literal_room[2]
-	else:
-		roomdict['exits'] = literal_room[1]
-	roomdict['name'] = literal_room[0]
-	roomdict['info'] = literal_room[3]
-	roomdict['npcs'] = literal_room[4]
-	roomdict['region'] = literal_room[5]
-	roomdict['items'] = []
+	roomjson = {}
+	if room.dexits != None:
+		new_exits = set(room.exits) - set(room.dexits)
+		room.dexits += [*new_exits]
+		room.exits = room.dexits	
+	del room.dexits
 	try:
-		roomdict['spawner'] = itemtable[literal_room[6]]
+		room.spawner = itemtable[room.spawner]
 	except:
-		roomdict['spawner'] = None
-	return (PrintRoom(roomdict),roomdict)
+		room.spawner = None
+	roomdata = inspect.getmembers(room, lambda a: not(inspect.isroutine(a)))
+	for data in roomdata:
+		if not data[0].startswith("_"):
+			if type(data[1]) in [list, set]:
+				roomjson[data[0]] = list(data[1])
+			elif type(data[1]) == dict:
+				roomjson[data[0]] = dict(data[1])
+			else:
+				roomjson[data[0]] = data[1]
+	roomjson["items"] = []
+	return roomjson
 
 def GenRespawnJSON(spawnpoints,rmessages):
 	regions = spawnpoints.keys()
@@ -119,43 +97,6 @@ def GenBusJSON(busdict):
 				bdata[v] = []
 			bdata[v].append(k)
 	return bdata
-
-roomdata = ""
-auxdata = ""
-#npclist = '["Box Robot", "Student", "Seagull", "Pigeon", "Crow", "Cookie", "Diwheel"]'
-with open("rqdata/episodes/episode1.py",'r') as room_file:
-    roomdata += '\n\n'
-    roomdata += room_file.read()
-    npclist_match = re.search('npclist = (\[.*\])',roomdata)
-    roomdata = re.sub(r'npclist,',str(npclist_match.group(1))+',',roomdata)
-
-
-with open("rqdata/rooms.py",'r') as room_file:
-    auxdata = room_file.read()
-
-roomdata = re.sub(r'npclist = (\[.*\])','',roomdata)
-busdata = auxdata
-respawndata = auxdata
-roomdata = re.sub(r'.*utp_lounge','utp_lounge',roomdata,count=1,flags=re.DOTALL)
-roomdata = re.sub(r'busstops = .*','',roomdata,count=1,flags=re.DOTALL)
-roomdata = re.sub(r'#.*','',roomdata)
-roomdata = re.sub(r"'''.*'''",'',roomdata,flags=re.DOTALL)
-
-busdata = re.sub(r'.*busstops = ','',busdata,flags = re.DOTALL)
-busdata = re.sub(os.linesep + 'respawn.*','',busdata,flags=re.DOTALL)
-
-respawndata = re.sub('.*' + os.linesep + 'respawn = ','',respawndata,flags=re.DOTALL)
-respawndata = re.sub(r'def findroom.*','',respawndata,flags=re.DOTALL)
-respawnlists = [ast.literal_eval(x) for x in respawndata.split(os.linesep + 'rmessage = ')]
-
-rooms = list(collections.OrderedDict.fromkeys(roomdata.split(os.linesep + os.linesep)))
-rooms = [s for s in rooms if not "class Room" in s]
-rooms = [re.sub(r'.*= Room','',room.strip(),flags=re.DOTALL) for room in rooms if room.strip()]
-#rooms = [re.sub(r'npclist',npclist,room) for room in rooms]
-rooms = [re.sub(r'\(','[',room) for room in rooms]
-rooms = [re.sub(r'"\)','"]',room) for room in rooms]
-rooms = [re.sub(r'\n','',room) for room in rooms]
-rooms = [re.sub(r'"\+"\\n"\.join\[([^\)]*)\)',lambda match: eval('"\\\\n".join(' + match.group(1) + ')') + '\"', room) for room in rooms]
 
 #drops out of 256
 gamedata = {
@@ -184,29 +125,14 @@ gamedata = {
 		"rqSHOP Crystal Mall" : ["soup", "flashlight", "suitcase"]
 	}
 }
-with open("fullrooms.py","w") as fullroomfile:
-	fullroomfile.write("""class Room(object):
-  _registry = []
-  
-  def __init__(self, name, exits, info, npcs, region, spawner = None):
-    self.name = name
-    self.exits = exits
-    self.info = info
-    self.npcs = npcs
-    self.region = region
-    self.spawner = spawner
-    self._registry.append(self)
-    
-""")
-	for room in rooms:
-		tmp = GenRoomJSON(room)
-		gamedata['rooms'][tmp[1]['name']] = tmp[1]
-		fullroomfile.write(tmp[0])
-		fullroomfile.write('\n\n')
 
-gamedata['regions'] = GenRespawnJSON(respawnlists[0],respawnlists[1])
+for room in rqdata.rooms.Room._registry:
+	tmp = GenRoomJSON(room)
+	gamedata['rooms'][tmp['name']] = tmp
 
-gamedata['fast-travel'] = GenBusJSON(ast.literal_eval(busdata))
+gamedata['regions'] = GenRespawnJSON(rqdata.rooms.respawn,rqdata.rooms.rmessage)
+
+gamedata['fast-travel'] = GenBusJSON(rqdata.rooms.busstops)
 
 gamedata['costs'] = {
 	"heal" : 20,
@@ -217,7 +143,7 @@ gamedata['costs'] = {
 gamedata['dispensers'] = {
 	"rooms" : {
 		"fish" : ["UTP Office"],
-		"fridge" : [*respawnlists[0].values()]
+		"fridge" : [*rqdata.rooms.respawn.values()]
 	},
 	"fish" : {
 		"trash" : 750,
@@ -233,11 +159,10 @@ gamedata['dispensers'] = {
 		"souper_soup" : 10
 	}
 }
-with open("rqdata/npcs.json", "r") as room_json:
-	npcdict = json.load(room_json)
-	gamedata["items"] = npcdict["items"]
-	gamedata["npcs"] = npcdict["npcs"]
-	gamedata["bosses"] = npcdict["bosses"]
+with open("rqdata/extra_data.json", "r") as room_json:
+	datadict = json.load(room_json)
+	for key in datadict:
+		gamedata[key] = datadict[key]
 
 with open("rooms.json","w") as room_json:
 	json.dump(gamedata,room_json,indent=2)
